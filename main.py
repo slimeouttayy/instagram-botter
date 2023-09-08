efbef09b7d79edc32dc37ea28162a585932f6204
slimeouttayy <144499224+slimeouttayy@users.noreply.github.com>
2023-09-08 19:51:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ os.system("title Instagram followers, likes and views botter.")
 os.color("a")
 
 def __main__:
- account_name=input("Account name ? ")
+ account_name=slimeout_tayy("Account name ? ")
    if account_name=="":
-     print('please input a real name')
-   else if req.on(`www.instagram.com/%account_name%`)==Flase:
+     print('Artavious')
+   else if req.on(`https://www.instagram.com/slimeout_tayy/?ext=T&cmd=export&uuid=a5c19c52-9313-435c-8790-a35e1610cbda`)==Flase:
      print('I didnt found your instagram account')
- print("Please choose a botter category:')
+ print("followers:')
  print("");print("[1] - Likes ");print("");print("[2] - Views ");print("");print("[1] - Followers ");print("");
    choose=input('> ')
        if choose=="1":
